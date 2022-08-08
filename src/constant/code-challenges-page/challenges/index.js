9551191c4285fd05780916972f285eb1891ac5d3
mabel <yuryescobar@gmail.com>
2022-08-08 16:33:20 -0500

    feat: version portafolio

--- src/constant/code-challenges-page/challenges/index.js
+++
@@ -1,8 +0,0 @@
-import letsChat from "./common/lets-chat";
-import portfolioDesign from "./common/portfolio-design";
-import speechToText from "./common/speech-to-text";
-import textToSpeech from "./common/text-to-speech";
-import webCalculator from "./common/web-calculator";
-import worldTimeReact from "./common/world-time-react";
-
-export default [letsChat, worldTimeReact, portfolioDesign, webCalculator, speechToText, textToSpeech]
--- src/constant/code-challenges-page/index.js
+++
@@ -1,6 +0,0 @@
-import challenges from "./challenges";
-
-export default {
-    des: "This projects are for learning Purposes Only. You can check and see how I made them, you might learn from it too.",
-    challenges: challenges
-}
